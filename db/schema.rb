1c3a4dd5ce2865d17c8013ec70bcd00ab8262a5c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 01:35:12 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 3) do
+
+  create_table "costume_stores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "still_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+  end
+
+  create_table "costumes", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.string "image_url"
+    t.integer "size"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "haunted_houses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.boolean "family_friendly?"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+    t.string "description"
+  end
 
 end

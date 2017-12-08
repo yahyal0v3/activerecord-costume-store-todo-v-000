186fea525b718ae86c809eb4a584af0324938163
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 01:33:01 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  def change
+    create_table :haunted_houses do |h|
+      h.string :name
+      h.string :location
+      h.integer :costume_inventory
+      h.integer :num_of_employees
+      h.boolean :still_in_business
+      h.datetime :opening_time
+      h.datetime :closing_time
+    end
+  end
+end

cff99cfa181a6deee77d8c5dfa9ab209dee85b8e
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 01:29:24 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -7,8 +7,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       c.integer :costume_inventory
       c.integer :num_of_employees
       c.boolean :still_in_business
-      c.string :opening_time
-      c.string :closing_time
+      c.datetime :opening_time
+      c.datetime :closing_time
     end
   end
 end

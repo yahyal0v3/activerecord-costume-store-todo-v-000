bc1e5312aa86bdff168297f7ae66ffc717c56f7c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 01:38:01 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -8,7 +8,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       h.integer :price
       h.boolean :family_friendly
       h.datetime :opening_date
-      h.datetime :closing_date 
+      h.datetime :closing_date
       h.string :description
     end
   end
